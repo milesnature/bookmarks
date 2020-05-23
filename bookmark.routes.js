057500e899c3098e348be290841be897852c5570
Miles Nature <milesnature@protonmail.com>
2020-05-22 21:54:03 -0700

    Naming convention, comment, error message, and misc cleanup

--- bookmark.routes.js
+++ bookmark.routes.js
@@ -1,24 +1,26 @@
 module.exports = (app) => {
+
     const bookmark = require('./bookmark.controller.js');
 
     // Create a new Bookmark
-    app.post('/bookmarks', bookmark.create);
+    app.post('/bookmarks', bookmark.createBookmark);
 
     // Retrieve all Bookmarks
-    app.get('/bookmarks', bookmark.findAll);
+    app.get('/bookmarks', bookmark.getBookmarks);
 
     // Retrieve all Bookmarks by group
-    app.get('/bookmarks/group/:group', bookmark.findAllByGroup);
+    app.get('/bookmarks/group/:group', bookmark.getBookmarksByGroup);
 
     // Retrieve a single Bookmark with bookmarkId
-    app.get('/bookmarks/:bookmarkId', bookmark.findOne);
+    app.get('/bookmarks/:bookmarkId', bookmark.getBookmark);
 
-    // Update a Note with bookmarkId
-    app.put('/bookmarks/:bookmarkId', bookmark.update);
+    // Update a Bookmark with bookmarkId
+    app.put('/bookmarks/:bookmarkId', bookmark.updateBookmark);
 
-    // Delete a Note with bookmarkId
-    app.delete('/bookmarks/:bookmarkId', bookmark.delete);
+    // Delete a Bookmark with bookmarkId
+    app.delete('/bookmarks/:bookmarkId', bookmark.deleteBookmark);
 
-    // Delete all bookmarks by their shared group name
+    // Delete a Group and all bookmarks associated to it
     app.delete('/bookmarks/group/:group', bookmark.deleteGroup);
+    
 }
