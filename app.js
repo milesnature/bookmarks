5d7e4e8caa4c8cde7606ad86d4894e9622bf27d9
Miles Nature <milesnature@protonmail.com>
2020-05-23 10:43:01 -0700

    Deployed on remote server. Fixed cache issues. Added password protection and HTTPS.

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');    
     next();
 });
 
