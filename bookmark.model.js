6ca608f2513d081b8a0fe692faf9fc7010b9650a
Miles Nature <milesnature@protonmail.com>
2020-05-26 10:17:25 -0700

    minlength to model

--- bookmark.model.js
+++ bookmark.model.js
@@ -5,30 +5,34 @@ const BookmarkSchema = mongoose.Schema({
     	type: String,
     	required: true,
     	unique: true,
+        minlength: 1,
         maxlength: 100
     },
     url : {
     	type: String,
     	required: true,
     	unique: true,
+        minlength: 1,   
         maxlength: 2083
     },
     group : {
     	type: String,
     	required: true,
     	unique: true,
+        minlength: 1,
         maxlength: 100
     },
     parent : {
     	type: String,
     	required: false,
     	unique: false,
+        minlength: 1,
         maxlength: 100
     },
     target : {
     	type: String,
     	required: false,
-    	unique: false,
+        minlength: 1,
         maxlength: 20
     }
 }, {
