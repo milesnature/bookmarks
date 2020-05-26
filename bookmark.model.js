f871a58131af27156cbd58b2177571208e2d5a9b
Miles Nature <milesnature@protonmail.com>
2020-05-26 09:38:30 -0700

    Form validation for length. UUID and session setup started.

--- bookmark.model.js
+++ bookmark.model.js
@@ -4,27 +4,32 @@ const BookmarkSchema = mongoose.Schema({
     name : {
     	type: String,
     	required: true,
-    	unique: true
+    	unique: true,
+        maxlength: 100
     },
     url : {
     	type: String,
     	required: true,
-    	unique: true
+    	unique: true,
+        maxlength: 2083
     },
     group : {
     	type: String,
     	required: true,
-    	unique: true
+    	unique: true,
+        maxlength: 100
     },
     parent : {
     	type: String,
     	required: false,
-    	unique: false
+    	unique: false,
+        maxlength: 100
     },
     target : {
     	type: String,
     	required: false,
-    	unique: false
+    	unique: false,
+        maxlength: 20
     }
 }, {
     timestamps: true
