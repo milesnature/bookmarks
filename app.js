446334fb90d4f7ae09ece7f43308e01ba2aad9fb
Miles Nature <milesnature@protonmail.com>
2020-05-26 19:59:19 -0700

    Parcel out bookmark creation into separate functions to facilitate building nested groups. Misc cleanup and consolidation.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const uuid       = require('uuid/v4');
 const session    = require('express-session');
 const FileStore  = require('session-file-store')(session);
 
-
 const app        = express();
 
 // Parse requests
