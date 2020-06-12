56de1c3ac3041ea10de82b4fdf87d75610ffd853
Miles Nature <milesnature@protonmail.com>
2020-06-11 19:49:11 -0700

    Saved for later

---
+++ public/assets/js/validate-url-field.js
@@ -0,0 +1,11 @@
+    function validate() {
+        var url = document.getElementById("url").value;
+        var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        if (pattern.test(url)) {
+            alert("Url is valid");
+            return true;
+        } 
+            alert("Url is not valid!");
+            return false;
+ 
+    }
