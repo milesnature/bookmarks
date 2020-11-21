7bc60d947009bd8be4579cd05ef60f659050d5dc
Miles Nature <email@milesnature.com>
2020-11-20 19:31:50 -0800

    New bouncing bookmark as loading animation.

--- public/assets/js/modules/formControl.js
+++ public/assets/js/modules/formControl.js
@@ -5,21 +5,46 @@ import { toggleFormGroups }                          from './formGroups.js';
 const 
 	formController = ( action, element ) => {
 
-		const edit = ( action, element ) => {
-			toggleFormGroups( 'remove' );
-			toggleFormSettings( 'remove' );
-			toggleFormEdit( 'add' );
-			const 
-				a = ( action )  ? document.querySelector( 'input[value=' + action  + ']' ) : '',
-				e = ( element ) ? document.querySelector( 'input[value=' + element + ']' ) : '';
-			if ( a ) { 
-				a.checked = true; 
-				if ( !e ) { a.focus(); }
-			}
-			if ( e ) { e.checked = true; }
-			actionState();
-			elementState();			
-		};
+		const 
+			edit = ( action, element ) => {
+				toggleFormGroups( 'remove' );
+				toggleFormSettings( 'remove' );
+				toggleFormEdit( 'add' );
+				const 
+					a = ( action )  ? document.querySelector( 'input[value=' + action  + ']' ) : '',
+					e = ( element ) ? document.querySelector( 'input[value=' + element + ']' ) : '';
+				if ( a ) { 
+					a.checked = true; 
+					if ( !e ) { a.focus(); }
+				}
+				if ( e ) { e.checked = true; }
+				actionState();
+				elementState();			
+			},
+
+			elem = document.documentElement,
+
+			/* View in fullscreen */
+			openFullscreen = () => {
+				if (elem.requestFullscreen) {
+					elem.requestFullscreen();
+				} else if (elem.webkitRequestFullscreen) { /* Safari */
+					elem.webkitRequestFullscreen();
+				} else if (elem.msRequestFullscreen) { /* IE11 */
+					elem.msRequestFullscreen();
+				}
+			},
+
+			/* Close fullscreen */
+			closeFullscreen = () => {
+				if (document.exitFullscreen) {
+					document.exitFullscreen();
+				} else if (document.webkitExitFullscreen) { /* Safari */
+					document.webkitExitFullscreen();
+				} else if (document.msExitFullscreen) { /* IE11 */
+					document.msExitFullscreen();
+				}
+			};
 
 		switch ( action ) {
 			case 'settings':
@@ -27,7 +52,7 @@ const
 				toggleFormGroups( 'remove' );
 				toggleFormSettings( 'add' );
 				document.querySelector( 'input' ).focus();	
-				import( './footer.js' ).then( ( module ) => { module.updateFooterButtons( 'settings' ); } );			
+				import( './footer.js' ).then( ( module ) => { module.updateFooterButtons( 'settings' ); } );		
 				break;
 			case 'create':
 				edit( action, element );
@@ -53,6 +78,8 @@ const
 		}
 
 		window.scrollTo( 0, document.body.scrollHeight );
+
+		// openFullscreen();
 
 	}
 
